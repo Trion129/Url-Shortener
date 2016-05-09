594fe3a1f172e82a63c2d7c7969c17a715f45137
trion129 <parmsingh129@yahoo.com>
2016-05-09 11:56:30 +0530

    going to work now

--- index.js
+++ index.js
@@ -5,34 +5,46 @@ var app = express();
 
 app.get("/:param(*)",function(req,res){
 	if(req.params.param.match(/^http:\/\/.*\.com\/?.*/i)){
-		res.end("process.env.MONGO_URI");
 		mongodb.MongoClient.connect(process.env.MONGO_URI || "mongodb://trion:paintball@ds017432.mlab.com:17432/mymongo"
 			,function(err,db){
 				if(err){
 					res.end("Error connecting to Database");
 					return;
 				}
-				db.urls.insert({
-					_id : shortid.generate(),
-					url : req.params.param
-				});
-				res.end(JSON.stringify({
-					_id : shortid.generate(),
-					url : req.params.param
-				}));
+				var urls = db.collection("urls");
+				var genid = shortid.generate();
+				urls.find({url: req.params.param}).limit(1).next(function(err,data){
+					if(!data){
+						urls.insert({
+							_id : genid,
+							url : req.params.param
+						});
+						res.end(JSON.stringify({
+							_id : genid,
+							url : req.params.param
+						}));
+						return;
+					}
+					res.end(JSON.stringify(data));
+				})
 			});
 	}
-	else if(req.params.param.match(/^[0-9]+$/i)){
+	else if(shortid.isValid(req.params.param)){
 		mongodb.MongoClient.connect(process.env.MONGO_URI || "mongodb://trion:paintball@ds017432.mlab.com:17432/mymongo"
 			,function(err,db){
 				if(err){
 					res.end("Error connecting to database");
 					return;
 				}
-				var myCursor = db.urls.find({_id: req.params.param});
-				var arrayed = myCursor.toArray();
-				res.redirect(arrayed[0].url);
-				res.end();
+				var urls = db.collection("urls");
+				urls.find({_id: req.params.param}).limit(1).next(function(err,data){
+					if(err){
+						res.end("Error getting data");
+						return;
+					}
+					res.redirect(data.url);
+					res.end();
+				});
 			});
 	}
 	else{
